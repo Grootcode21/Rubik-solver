51939b737e88c7bc46ba307ff87122133852cb9f
Grootcode21 <antonyndichu15@gmail.com>
2025-11-09 22:49:12 +0300

    color map

--- code.py
+++ code.py
@@ -1,7 +1,24 @@
 #pip install rubik-solver
+#pip install pandas
+#pip install matplotlib
 
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from typing import Dict, List
 import time
+
+try:
+    from rubik_solver import utils as rubik_utils
+except Exception:
+    rubik_utils = None
+
+# Define color map for faces
+COLOR_MAP = {
+    'U': 'white',
+    'R': 'red',
+    'F': 'green',
+    'D': 'yellow',
+    'L': 'orange',
+    'B': 'blue'
+}
